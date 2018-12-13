26e607e209c51e48bc77adc5fad2e5ace34d66c1
Lucian Sturião <lucian.sturiao@zoom.com.br>
2018-12-13 19:58:44 -0200

    creating modal

--- src/index/components/page1/Page1.js
+++ src/index/components/page1/Page1.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button, Container, Row, Col } from 'reactstrap';
+import { Button, Container, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Editor from '../shared/editor/Editor';
 
 class Page1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
     render() {
 
         const page1ContainerStyle = {
@@ -12,7 +27,7 @@ class Page1 extends Component {
         const page1FirstRowStyle = {
             paddingBottom: 10
         };
-        
+
         var htmlCode =
             `<!DOCTYPE html>
 <html>
@@ -28,7 +43,7 @@ class Page1 extends Component {
             <div style={page1ContainerStyle}>
                 <Container>
                     <Row style={page1FirstRowStyle}>
-                        <Col><Button>Teste</Button></Col>
+                        <Col><Button onClick={this.toggle}>Visualizar</Button></Col>
                     </Row>
                     <Row>
                         <Col>
@@ -39,6 +54,17 @@ class Page1 extends Component {
                         </Col>
                     </Row>
                 </Container>
+
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
 
             </div>
         );
