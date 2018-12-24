889c6f0c65b5b5576a0143686c8464ee54c73a01
Lucian Sturião <luciansr@gmail.com>
2018-12-24 00:32:51 -0200

    changing post parameters

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL
 
 export const getAuthToken = (username, password) => dispatch => {
-    axios.post(`${API_URL}/auth/token`, {
-        username: username,
-        password: password
-    }).then(data => dispatch({
-        type: GET_AUTH_TOKEN,
-        payload: data
-    }))
+    axios.post(`${API_URL}/auth/token?username=${username}&password=${password}`, null)
+        .then(data => dispatch({
+            type: GET_AUTH_TOKEN,
+            payload: data
+        }));
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer'
 
@@ -9,5 +9,10 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer, 
     initialState, 
-    applyMiddleware(...middleware));
+    process.env.NODE_ENV == `development` ? 
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        ): applyMiddleware(...middleware)
+    );
 export default store;
